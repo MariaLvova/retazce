82501e79eb5205116fa914ae0e7f6fd1563109d6
MariaLvova <lvovamaria0202@gmail.com>
2024-06-23 01:01:17 +0200

    Implementácia základnej funkcie na generovanie kombinácií

--- retazce/main.c
+++ retazce/main.c
@@ -1,1 +1,14 @@
-﻿
+﻿#include <stdio.h>
+
+void generateCombinations(char* result, int index, int n, int l) {
+    if (index == l) {
+        result[index] = '\0';
+        printf("%s\n", result);
+        return;
+    }
+
+    for (char c = 'a'; c < 'a' + n; c++) {
+        result[index] = c;
+        generateCombinations(result, index + 1, n, l);
+    }
+}
